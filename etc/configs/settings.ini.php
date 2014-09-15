01b2517952fa86c21301ef3d749f36b6bd485cc8
Oshane Bailey <b4.oshany@gmail.com>
2014-09-14 22:48:09 -0500

    Added the config files.

--- etc/configs/settings.ini.php
+++ etc/configs/settings.ini.php
@@ -1,12 +1,16 @@
 <?php
 use libs\vecni\Vecni;
+use libs\mongodb\Model;
+
 # name of the website
-Vecni::$BRAND_NAME = 'Ohomes';
+Vecni::$BRAND_NAME = 'Vecni';
 # company location
 Vecni::$company_address = 'Kingston, Jamaica';
 # company contact number
 Vecni::$company_number = '(876) 8295969';
 # company email address
-Vecni::$company_email = 'info@feroinc.com';
-Vecni::$company_name = 'Fero Inc.';
+Vecni::$company_email = 'b4.oshany@gmail.com';
+Vecni::$company_name = 'Osoobe';
+
+Model::$db = "vecni";
 ?>
